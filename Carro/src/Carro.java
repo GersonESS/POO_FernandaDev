2dfda0d6028976f60c17fd698e54d9d92e0bd3d2
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-04-04 21:55:25 -0300

    qaa

--- Carro/src/Carro.java
+++ Carro/src/Carro.java
@@ -4,8 +4,29 @@ public class Carro {
     int ano;
     int vel;
 
+    // construtores padrao
+    Carro() {
+
+    }
+
+    Carro(String nome) {
+        this.nome = nome;
+    }
+
+    Carro(String nome, String marca) {
+        this.nome = nome;
+        this.marca = marca;
+    }
+
+    Carro(String nome, String marca, int vel) {
+        this.nome = nome;
+        this.marca = marca;
+        this.vel = vel;
+    }
+
+    // System.out.println("A velocidade é " + c1.vel + "KM/H");
     // metodo
-    void acelarar(int aceleracao) {
+    void acelerar(int aceleracao) {
         vel += aceleracao;
     }
 
--- Carro/src/CarroTestar.java
+++ Carro/src/CarroTestar.java
@@ -1,7 +1,18 @@
 public class CarroTestar {
     public static void main(String[] args) {
         Carro c1 = new Carro();
-        c1.nome = "UNO";
+        c1.nome = "UNO    ";
         c1.marca = "Fiat";
+        c1.vel = 60;
+        c1.acelerar(10);
+        // System.out.println("A velocidade é " + c1.vel + "KM/H");
+        System.out.println("nome: " + c1.nome + " Marca: " + c1.marca + " Vel: " + c1.vel);
+        c1.freiar(30);
+        // System.out.println("A velocidade é " + c1.vel + "KM/H");
+        Carro c2 = new Carro("Santana", "VW  ");
+        c2.vel = 15;
+        System.out.println("nome: " + c2.nome + " Marca: " + c2.marca + " Vel: " + c2.vel);
+        Carro c3 = new Carro("Ferrari", "FORD", 90);
+        System.out.println("nome: " + c3.nome + " Marca: " + c3.marca + " Vel: " + c3.vel);
     }
 }
